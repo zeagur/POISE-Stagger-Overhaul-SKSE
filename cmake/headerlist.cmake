66207b8e83d3a48c97bb5efff62509a9ec18a28e
Rujrawee Komolsai <rujrawee_k@outlook.com>
2024-05-29 15:49:20 +0700

    remove submodules maintainance from repo, remove version header from repo, update vcpkg info and simplified CmakePresets

--- cmake/headerlist.cmake
+++
@@ -1,8 +0,0 @@
-set(headers ${headers}
-	src/Loki_PluginTools.h
-	src/PCH.h
-	src/POISE/PoiseMod.h
-	src/POISE/TrueHUDAPI.h
-	src/POISE/TrueHUDControl.h
-	src/ActorCache.h
-)
--- cmake/sourcelist.cmake
+++
@@ -1,8 +0,0 @@
-set(sources ${sources}
-	src/Loki_PluginTools.cpp
-	src/main.cpp
-	src/PCH.cpp
-	src/POISE/PoiseMod.cpp
-	src/POISE/TrueHUDControl.cpp
-	src/ActorCache.cpp
-)
